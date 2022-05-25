3ce827c30f65493c00f3b79612fa5aadd514e45c
Nikhil <nikhilmalewar21@gmail.com>
2022-05-26 02:12:44 +0530

    edit im table errors fix

--- src/app/vendor/vendor-finance/edit-im/edit-im.component.ts
+++ src/app/vendor/vendor-finance/edit-im/edit-im.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute } from '@angular/router';
 import { ImService } from 'app/vendor/services/im.service';
 import { Subscription } from 'rxjs';
@@ -16,9 +17,50 @@ export class EditImComponent implements OnInit {
   panelOpenStateDetails = true;
   panelOpenStateAccDetails = true;
   panelOpenStateTxnProfile = true;
+  dataSourceNew: MatTableDataSource<any>;
+
+  dataColumns = [
+    'name',
+    'quantity',
+    'size',
+    'code'
+  ];
+
+  labels = [
+    'Debit Processing Charge Account',
+    'Debit Commission Charge Account',
+    'Debit Interest Account',
+    'Debit Account',
+  ];
+
+  data = [
+    {
+      name: '100082624363699',
+      quantity: 10999849839849,
+      size: '1000',
+      code: '1000'
+    },
+    {
+      name: '100082624363699',
+      quantity: 10999849839849,
+      size: '1000',
+      code: '1000'
+    },
+    {
+      name: '100082624363699',
+      quantity: 10999849839849,
+      size: '1000',
+      code: '1000'
+    }
+  ];
+
+  displayedColumnsNew = [];
   constructor(
     private _activatedRoute:ActivatedRoute,
-    private _imService: ImService) { }
+    private _imService: ImService) { 
+      this.transpose();
+      this.fillLabels();
+    }
 
   ngOnInit() {
     this.subscriptions.push(
@@ -29,6 +71,26 @@ export class EditImComponent implements OnInit {
          this.im = industryMajors.find(im => im.imCode == this.id);    
      })
     )
+  }
+
+  transpose() {
+    let transposedData = [];
+    for (let column = 0; column < this.dataColumns.length; column++) {
+      transposedData[column] = {
+        label: this.labels[column]
+      };
+      for (let row = 0; row < this.data.length; row++) {
+        transposedData[column][`column${row}`] = this.data[row][this.dataColumns[column]];
+      }
+    }
+    this.dataSourceNew = new MatTableDataSource(transposedData);
+  }
+
+  fillLabels() {
+    this.displayedColumnsNew = ['label'];
+    for (let i = 0; i < this.data.length; i++) {
+      this.displayedColumnsNew.push('column' + i);
+    }
   }
 
 }
